cfbebb2c241b5b8c3ee2d466ce75db5d503f836e
michalAdmin <112867735+michalAdmin@users.noreply.github.com>
2023-06-12 01:05:48 +0300

    Initial commit

---
+++ server/models/ChatRoom.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const ChatRoomSchema = mongoose.Schema(
+  {
+    members: Array,
+  },
+  { timestamps: true }
+);
+
+const ChatRoom = mongoose.model("ChatRoom", ChatRoomSchema);
+
+export default ChatRoom;
+ 
